338b79c5d6399aa4100f726faf3d88ece9ceedc7
Jeferson Macedo <jhef.salles@gmail.com>
2026-06-18 01:17:29 -0300

    Implement AddSubTask command, handler, validator, and response model

---
+++ microservices/TODO-App/src/TodoApp.Domain/Entities/SubTask.cs
@@ -0,0 +1,13 @@
+namespace TodoApp.Domain.Entities;
+
+public class SubTask
+{
+    public Guid Id { get; set; }
+    public Guid TaskId { get; set; }
+    public string Title { get; set; } = null!;
+    public string? Description { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public TaskItem Task { get; set; } = null!;
+}
